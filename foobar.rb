200656b21112295ef577fe232f56e050e8c8c1c8
Jennifer Grannen <jenngrannen@berkeley.edu>
2018-09-17 15:57:47 -0700

    Q3, Q4 done

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,13 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    total = 0
+    for elem in a do
+      elem = elem.to_i + 2
+      if elem % 2 == 0 and elem < 10
+        total = total + elem
+      end
+    end
+    total
   end
 end
