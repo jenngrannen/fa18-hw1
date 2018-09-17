169f489a629efc33039fd2847850a1e020f76398
Jennifer Grannen <jenngrannen@berkeley.edu>
2018-09-17 15:40:59 -0700

    Q1, Q2 done'

--- hw1.rb
+++ hw1.rb
@@ -1,11 +1,11 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a = a.sort.map{ |x| x + 1}
 end
 
 def combine_name(first_name, last_name)
